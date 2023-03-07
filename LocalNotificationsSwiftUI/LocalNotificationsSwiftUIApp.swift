81d6f5ccbbb2aeeebdf10f44a60f2ec2f1b56245
Igor Sysoev <igro.sia@yandex.ru>
2023-03-07 20:11:06 +0300

    Initial Commit

---
+++ LocalNotificationsSwiftUI/LocalNotificationsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationsSwiftUIApp.swift
+//  LocalNotificationsSwiftUI
+//
+//  Created by Игорь Сысоев on 07.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
