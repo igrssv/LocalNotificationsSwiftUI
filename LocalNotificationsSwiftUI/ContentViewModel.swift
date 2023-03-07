6ef912ec566684ef8c08505e5d5bae344f00899b
Igor Sysoev <igro.sia@yandex.ru>
2023-03-07 21:14:53 +0300

    requestAuthorization

--- LocalNotificationsSwiftUI/ContentViewModel.swift
+++ LocalNotificationsSwiftUI/ContentViewModel.swift
@@ -6,9 +6,26 @@
 //
 
 import Foundation
+import UserNotifications
 
 class ContentViewModel: ObservableObject {
     @Published var message = ""
     @Published var time = Date()
+    private var center = UNUserNotificationCenter.current()
     
+    init() {
+        requestNotification()
+    }
+    
+    private func requestNotification() {
+        center.requestAuthorization(options: [.alert, .sound, .badge])  { success, error in
+            DispatchQueue.main.async {
+                if success {
+                    print("success")
+                } else if let error = error {
+                    print(error)
+                }
+            }
+        }
+    }
 }
